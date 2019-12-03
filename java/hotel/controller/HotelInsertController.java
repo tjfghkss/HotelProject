9b30245bef49a9919c3f84bede71373bf5dcb739
ByoungHwaLee <49489205+ByoungHwaLee@users.noreply.github.com>
2019-12-03 18:49:07 +0900

    Add files via upload

---
+++ java/hotel/controller/HotelInsertController.java
@@ -0,0 +1,21 @@
+package hotel.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+public class HotelInsertController {
+	
+	private final String command="/insert.ho";
+	private final String getPage="hotelInsertForm";
+	
+	@RequestMapping(command)
+	public String insert(HttpSession session) {
+		
+		return getPage;
+	}
+}
